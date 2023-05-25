ed927dc1b8cd847a159faff3b44842419e0e231d
orxanmuradli <orxanmuradli1907@gmail.com>
2023-05-25 15:08:44 +0400

    some changes

--- src/components/Pages/Home/PopularPage/PopularPage.jsx
+++ src/components/Pages/Home/PopularPage/PopularPage.jsx
@@ -1,41 +1,43 @@
-import sushi from '../../../../assets/sushi.png';
-import noodle from '../../../../assets/noodle.png';
-import spring from '../../../../assets/spring.png';
-import "./PopularPage.css"
-import React from 'react';
-
+import sushi from "../../../../assets/sushi.png";
+import noodle from "../../../../assets/noodle.png";
+import spring from "../../../../assets/spring.png";
+import "./PopularPage.css";
+import React from "react";
 
 const PopularPage = () => {
   return (
     <div className="popular">
       <div className="popular-tittle">
-       
-       <h1>POPULAR</h1>
-       <div className="cards">
-        <div className='cart'>
-        <img src={sushi} alt="" />
-        <h2>Nobel Set</h2>
-        <p>DÜYÜ, NORİ, XİYAR </p>
-        <span>32 AZN</span>
+        <h1>POPULAR</h1>
+        <div className="cards">
+          <div className="cart">
+            <img src={sushi} alt="" />
+            <div>
+              <h2>Nobel Set</h2>
+              <p>DÜYÜ, NORİ, XİYAR </p>
+              <span>32 AZN</span>
+            </div>
+          </div>
+          <div className="cart">
+            <img src={noodle} alt="" />
+            <div>
+              <h2>Seafood Noodle</h2>
+              <p>DÜYÜ, NORİ, XİYAR </p>
+              <span>32 AZN</span>
+            </div>
+          </div>
+          <div className="cart">
+            <img src={spring} alt="" />
+            <div>
+              <h2>Spring Roll Salmon</h2>
+              <p>DÜYÜ, NORİ, XİYAR </p>
+              <span>32 AZN</span>
+            </div>
+          </div>
         </div>
-       <div className='cart'>
-       <img src={noodle} alt="" />
-       <h2>Seafood Noodle</h2>
-       <p>DÜYÜ, NORİ, XİYAR </p>
-       <span>32 AZN</span>
-       </div>
-       <div className='cart'>
-       <img src={spring} alt="" />
-       <h2>Spring Roll Salmon</h2>
-       <p>DÜYÜ, NORİ, XİYAR </p>
-       <span>32 AZN</span>
-       </div>
-       
-       </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PopularPage
-
+export default PopularPage;
--- src/components/Pages/Menu/Menu.jsx
+++ src/components/Pages/Menu/Menu.jsx
@@ -1,70 +1,104 @@
-import "./Menu.css"
-import menu1 from '../../../assets/menu1.png';
-import menu2 from '../../../assets/menu2.png';
-import menu3 from '../../../assets/menu3.png';
-import menu4 from '../../../assets/menu4.png';
-import menu5 from '../../../assets/menu5.png';
-import menu6 from '../../../assets/menu6.png';
+import "./Menu.css";
+import menu1 from "../../../assets/menu1.png";
+import menu2 from "../../../assets/menu2.png";
+import menu3 from "../../../assets/menu3.png";
+import menu4 from "../../../assets/menu4.png";
+import menu5 from "../../../assets/menu5.png";
+import menu6 from "../../../assets/menu6.png";
+import vector from "../../../assets/vector.png";
+import scor from "../../../assets/scor.png";
+import left from "../../../assets/left.png";
+import scor2 from "../../../assets/scor2.png";
 import React from "react";
 
 const Menu = () => {
   return (
-
     <div className="special">
       <div className="special-tittle">
-       
-       <h3>MENYU</h3>
-       <div className="special-bar">
-       <div className="specials">
-        <p>ROLLAR</p>
+        <div className="menu_title">
+          <div>
+            <h3>MENYU</h3>
+          </div>
+          <div className="menu-download">
+            <span>Menyunu yüklə</span>
+            <img src={scor2} alt="" />
+          </div>
         </div>
 
-        
-       <div className="special-cards">
-        <div className='cart'>
-        <img src={menu1} alt="" />
-        <h2>California Salmon</h2>
-        <p>DÜYÜ, NORİ, XİYAR </p>
-        <span>32 AZN</span>
+        <div className="special-bar">
+          {/* <div className="specials"> */}
+          {/* <p>ROLLAR</p> */}
+          {/* <div className="leftscor">
+            <img src={left} alt="" /> </div>
+            
+          </div> */}
+          <div className="left-container">
+            <p>ROLLAR</p>
+            <div className="scor scor-left">
+              <img src={left} alt="" />
+            </div>
+            <div className="scor scor-right">
+              <img src={scor} alt="" />
+            </div>
+            <div className="special-cards">
+              <div className="cart">
+                <img src={menu1} alt="" />
+                <h2>California Salmon</h2>
+                <p>DÜYÜ, NORİ, XİYAR </p>
+                <span>32 AZN</span>
+              </div>
+              <div className="cart">
+                <img src={menu2} alt="" />
+                <h2>Oasis Roll</h2>
+                <p>DÜYÜ, NORİ, XİYAR </p>
+                <span>32 AZN</span>
+              </div>
+              <div className="cart">
+                <img src={menu3} alt="" />
+                <h2>Philadelphia Classic</h2>
+                <p>DÜYÜ, NORİ, XİYAR </p>
+                <span>32 AZN</span>
+              </div>
+              <div className="cart">
+                <img src={menu4} alt="" />
+                <h2>Unagi Classic</h2>
+                <p>DÜYÜ, NORİ, XİYAR </p>
+                <span>32 AZN</span>
+              </div>
+              <div className="cart">
+                <img src={menu5} alt="" />
+                <h2>Tuna Classic</h2>
+                <p>DÜYÜ, NORİ, XİYAR </p>
+                <span>32 AZN</span>
+              </div>
+              <div className="cart">
+                <img src={menu6} alt="" />
+                <h2>Mango Fusion</h2>
+                <p>DÜYÜ, NORİ, XİYAR </p>
+                <span>32 AZN</span>
+              </div>
+            </div>
+          </div>
+          <div className="cart-container">
+            <div className="wrapper-cart">
+              <h2>SETLƏR</h2>
+              <img src={vector} alt="" />
+            </div>
+
+            <div className="wrapper-cart">
+              <h1>ƏRİŞTƏLƏR & BURGER</h1>
+              <img src={vector} alt="" />
+            </div>
+
+            <div className="wrapper-cart">
+              <h1>İÇKİLƏR VƏ ƏLAVƏLƏR</h1>
+              <img src={vector} alt="" />
+            </div>
+          </div>
         </div>
-       <div className='cart'>
-       <img src={menu2} alt="" />
-       <h2>Oasis Roll</h2>
-       <p>DÜYÜ, NORİ, XİYAR </p>
-       <span>32 AZN</span>
-       </div>
-       <div className='cart'>
-       <img src={menu3} alt="" />
-       <h2>Philadelphia Classic</h2>
-       <p>DÜYÜ, NORİ, XİYAR </p>
-       <span>32 AZN</span>
-       </div>
-       <div className='cart'>
-       <img src={menu4} alt="" />
-       <h2>Unagi Classic</h2>
-       <p>DÜYÜ, NORİ, XİYAR </p>
-       <span>32 AZN</span>
-       </div>
-       <div className='cart'>
-       <img src={menu5} alt="" />
-       <h2>Tuna Classic</h2>
-       <p>DÜYÜ, NORİ, XİYAR </p>
-       <span>32 AZN</span>
-       </div>
-       <div className='cart'>
-       <img src={menu6} alt="" />
-       <h2>Mango Fusion</h2>
-       <p>DÜYÜ, NORİ, XİYAR </p>
-       <span>32 AZN</span>
-       </div>
-       </div>
-       
-       </div>
-       
       </div>
-     
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
