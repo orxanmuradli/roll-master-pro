0bfe356e25a0710c5db01d2cb66997b0605811d3
orxanmuradli <orxanmuradli1907@gmail.com>
2023-09-01 15:06:14 +0400

    fixed navbar sticky bug

--- src/components/Pages/Contact/Contact.jsx
+++ src/components/Pages/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Contact.module.css";
 import insta from "../../../assets/insta.png";
 import face from "../../../assets/face.png";
@@ -7,50 +7,99 @@ import wolt from "../../../assets/wolt.png";
 import { Link } from "react-router-dom";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    number: "",
+    text: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const validationErrors = {};
+    if (!formData.name.trim()) {
+      validationErrors.name = "Istifadəçi adı soyadi daxil edin";
+    }
+
+    if (!formData.number.trim()) {
+      validationErrors.number = "Nomre daxil edin";
+    }
+    if (!formData.text.trim()) {
+      validationErrors.text = "Isramic gonderin.";
+    }
+    setErrors(validationErrors);
+
+    console.log(formData);
+  };
   return (
     <section className={styles.sectioncontact}>
-      <div className={styles.contacttwo}>
-        <BreadCrumbs pathnames={["", "KAMPANYALAR"]} />
-      </div>
-      <h2>Əlaqə</h2>
-      <div className={styles.main}>
-        <div className={styles.leftside}>
-          <p>İrad və təklifləriniz</p>
-          <div className={styles.naming}>
-            <label for="">Ad və soyad</label> <br />
-            <input
-              type="text"
-              placeholder="Ad və soyad"
-              className={styles.inputss}
-            />
+      <form onSubmit={handleSubmit}>
+        <div className={styles.contacttwo}>
+          <BreadCrumbs pathnames={["", "KAMPANYALAR"]} />
+        </div>
+        <div className={styles.contacttwo_h2}>
+          <h2>Əlaqə</h2>
+        </div>
+        <div className={styles.main}>
+          <div className={styles.leftside}>
+            <p>İrad və təklifləriniz</p>
+            <div className={styles.naming}>
+              <label for="">Ad və soyad</label> <br />
+              <input
+                type="text"
+                placeholder="Ad və soyad"
+                className={styles.inputss}
+                onChange={handleChange}
+              />
+              {errors.name && <p>{errors.name}</p>}
+            </div>
+            <div className={styles.contact}>
+              <label for="">Əlaqə nömrəsi</label> <br />
+              <input
+                type="number"
+                placeholder="+994 "
+                className={styles.kjba}
+                onChange={handleChange}
+              />
+              {errors.number && <p>{errors.number}</p>}
+            </div>
+            <div className={styles.complain}>
+              <label for="">Şikayətiniz / Təklifiniz</label> <br />
+              <input
+                type="text"
+                placeholder="Şikayət və ya təklifinizi qeyd edin"
+                onChange={handleChange}
+              />
+              {errors.text && <p>{errors.text}</p>}
+            </div>
           </div>
-          <div className={styles.contact}>
-            <label for="">Əlaqə nömrəsi</label> <br />
-            <input type="number" placeholder="+994 " className={styles.kjba} />
-          </div>
-          <div className={styles.complain}>
-            <label for="">Şikayətiniz / Təklifiniz</label> <br />
-            <input
-              type="text"
-              placeholder="Şikayət və ya təklifinizi qeyd edin"
-            />
+          <div className={styles.rightside}>
+            <p>Sifariş üçün</p>
+            <div className={styles.wolt}>
+              <Link to="#">+994517365616</Link> <img src={wolt} alt="" />
+            </div>
+            <p>Sosial şəbəkə</p>
+            <div className={styles.sosials}>
+              <img src={insta} alt="" />
+              <img src={face} alt="" />
+            </div>
           </div>
         </div>
-        <div className={styles.rightside}>
-          <p>Sifariş üçün</p>
-          <div className={styles.wolt}>
-            <Link to="#">+994517365616</Link> <img src={wolt} alt="" />
-          </div>
-          <p>Sosial şəbəkə</p>
-          <div className={styles.sosials}>
-            <img src={insta} alt="" />
-            <img src={face} alt="" />
-          </div>
+        <div className={styles.bottomsside}>
+          <button className={styles.redsButton} type="submit">
+            GÖNDƏR
+          </button>
         </div>
-      </div>
-      <div className={styles.bottomsside}>
-        <button className={styles.redsButton}>GÖNDƏR</button>
-      </div>
+      </form>
     </section>
   );
 };
