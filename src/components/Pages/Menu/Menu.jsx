3c56066f1a9ce593aa5be8d18046bdd55ed7c027
agakhansr <agaxanist@gmail.com>
2023-09-05 14:09:14 +0400

    slider progress

--- src/components/Pages/Menu/Menu.jsx
+++ src/components/Pages/Menu/Menu.jsx
@@ -1,9 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./menu.styles.css";
-import vector from "../../../assets/vector.png";
-import scor from "../../../assets/scor.png";
 import left from "../../../assets/left.png";
-import gsap from "gsap";
 import ProductSlider from "./productSlider";
 import rollarData from "./menuDatas/rollarData";
 import setlerData from "./menuDatas/setlerData";
@@ -12,10 +9,15 @@ import ickilerData from "./menuDatas/ickilerData";
 
 const Menu = () => {
   const [activeTab, setActiveTab] = useState(1);
-  const activeTabRef = useRef(null); // Create a ref for the active tab element
+  const activeTabRef = useRef(null);
+  const handleTabClick = () => {
+   const row = document.querySelectorAll(".row");
 
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
+    row.forEach((row) => {
+      row.classList.add("activeTab");
+    }
+    );
+
   };
 
   const getDataForTab = () => {
@@ -34,38 +36,30 @@ const Menu = () => {
   };
 
   const inActiveTabName = ["rollar", "setler", "eristeler", "ickiler"];
-// wraps z-index if 3, container is 2
-//
+
+  const handleLeftButtonClick = () => {
+    setActiveTab((prevTab) => (prevTab === 1 ? 4 : prevTab - 1));
+
+  };
+
+  const handleRightButtonClick = () => {
+    setActiveTab((prevTab) => (prevTab === 4 ? 1 : prevTab + 1));
+  };
+
   return (
     <div className="menuSection container">
-      <ProductSlider products={getDataForTab()} className="sliderContainer"/>
+      <ProductSlider products={getDataForTab()} className="sliderContainer" />
       <div className="menuSliderButtons">
-        <div
-          className="menuSliderButtonsLeft"
-          onClick={() => {
-            setActiveTab(activeTab - 1);
-            if (activeTab === 1) {
-              setActiveTab(4);
-            }
-          }}
-        >
-          <img src={left} alt="vector"/>
+        <div className="menuSliderButtonsLeft" onClick={handleLeftButtonClick}>
+          <img src={left} alt="vector" />
         </div>
-        <div
-          className="menuSliderButtonsRight"
-          onClick={() => {
-            setActiveTab(activeTab + 1);
-            if (activeTab === 4) {
-              setActiveTab(1);
-            }
-          }}
-        >
+        <div className="menuSliderButtonsRight" onClick={handleRightButtonClick}>
           <img src={left} alt="vector" />
         </div>
       </div>
-      <div className="productWraps rows">
+      <div className="productWraps rows" onClick={handleTabClick}>
         {inActiveTabName.map((tabName, index) => (
-          <div key={index} class="row">{tabName}</div>
+          <div key={Math.random}  className="row">{tabName}</div>
         ))}
       </div>
     </div>
